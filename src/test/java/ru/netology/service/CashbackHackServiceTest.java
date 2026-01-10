80a1d80a014157a2e2d7f395088d3738ee11818c
Test learning <aleksandravelichko@mail.ru>
2026-01-10 18:31:51 +0300

    first commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn100IfAmount900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn0IfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn900IfAmount1100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(expected, actual);
+    }
+}
