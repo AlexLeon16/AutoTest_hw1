58007e4580840d53b2c49fde00954a06ae2679b2
AlexLeon16 <aleksandravelichko@mail.ru>
2026-03-26 12:08:49 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,62 +1,32 @@
 package ru.netology.service;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
     @Test
     public void shouldReturn100IfAmount900() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(100, service.remain(900));
+        assertEquals(service.remain(900), 100);
     }
 
     @Test
-    public void shouldReturn1000IfAmount1000() {
-        // ВНИМАНИЕ: Это демонстрация бага!
+    public void shouldReturn0IfAmount1000() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(0, service.remain(1000)); // Баг: должно быть 0
+        assertEquals(service.remain(1000), 0); // этот тест упадёт
     }
 
     @Test
     public void shouldReturn900IfAmount1100() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(900, service.remain(1100));
-    }
-
-    @Test
-    public void shouldReturn1000IfAmount0() {
-        CashbackHackService service = new CashbackHackService();
-        assertEquals(1000, service.remain(0)); // Баг: должно быть 0
-    }
-
-    @Test
-    public void shouldReturn1000IfAmount2000() {
-        CashbackHackService service = new CashbackHackService();
-        assertEquals(1000, service.remain(2000)); // Баг: должно быть 0
+        assertEquals(service.remain(1100), 900);
     }
 
     @Test
     public void shouldReturn1IfAmount999() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(1, service.remain(999));
-    }
-
-    @Test
-    public void shouldReturn500IfAmount500() {
-        CashbackHackService service = new CashbackHackService();
-        assertEquals(500, service.remain(500));
-    }
-
-    @Test
-    public void shouldReturn500IfAmount1500() {
-        CashbackHackService service = new CashbackHackService();
-        assertEquals(500, service.remain(1500));
-    }
-
-    @Test
-    public void shouldReturn999IfAmount1() {
-        CashbackHackService service = new CashbackHackService();
-        assertEquals(999, service.remain(1));
+        assertEquals(service.remain(999), 1);
     }
 }
